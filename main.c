7b70820ecbceb771dfd6a4faf95f2e3421d6cfc6
Jeck <zrkeimperial@gmail.com>
2024-04-30 18:08:20 +0800

    added skeleton src

---
+++ main.c
@@ -0,0 +1,34 @@
+  																							/*
+             _____      ___         _   _           ____                      
+            |     \ ___|  _|___ ___| |_|_|_ _ ___  |    \ ___ ___ ___ _ _ ___ 
+            |  |  | -_|  _| -_|  _|  _| | | | -_|  |  |  | -_|  _| . | | |_ -|
+            |____/|___|_| |___|___|_| |_|\_/|___|  |____/|___|___|___|_  |___|
+                                                                    |___|   
+    
+    Filename: main.c
+	Author: John Bicierro
+
+    Collaborators:
+        [+] Romar Castro
+        [+] Ken Lanon
+        [+] Eric Tan
+																							*/  
+
+#define VER 0.1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Socket
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello, world!");
+    return 0;
+}
